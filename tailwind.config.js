2a8768f6415545800d87abf3001e6908eaa6c6b9
Drapys <111556289+Drapys@users.noreply.github.com>
2024-05-06 20:09:42 +0200

    EVERYTHING DONE

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
     extend: {
       width:{
         '200%':'100rem',
+        '3xl':'768px',
+        '1024l':'1024px',
+        '1280l':'1280px',
+        '1536':'1536px',
+
+      },
+      height:{
+        '2xl':'667px',
+        '3xl':'768px',
+        '4xl':'1024px',
+        '5xl':'1280px',
+      },
+      top:{
+        '110':'100px',
+
       },
       backgroundImage: {
         'path': "url('../../wwwroot/Imgs/Path.png')",
